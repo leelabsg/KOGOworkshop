84b85408cbdd339e42d6e1d5147b0c66bc9c664f
Flumenlucidum <49855759+Flumenlucidum@users.noreply.github.com>
2021-02-24 20:53:46 +0900

    Update plots.R

--- SAIGE/plots.R
+++ SAIGE/plots.R
@@ -3,10 +3,10 @@ library(qqman)
 gwas<-read.table('finalresult.txt',h=T)
 
 colnames(gwas)[c(2,3,14)]<-c('BP','SNP','P')
-jpeg(file='manhattan_plot.jpeg',width=1000, height=1000)
+png(file='manhattan_plot.png',width=1000, height=1000)
 manhattan(gwas, main='Chromosome 1 Manhattan Plot')
 dev.off()
 
-jpeg(file='manhattan_plot.jpeg',width=1000, height=1000)
+png(file='manhattan_plot.png',width=1000, height=1000)
 qq(gwas$P, main='Q-Q plot of chromosome 1')
 dev.off()
