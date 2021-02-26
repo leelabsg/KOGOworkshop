3af28992edd35ddf84088ef20a0fa4b754dfaddd
Flumenlucidum <49855759+Flumenlucidum@users.noreply.github.com>
2021-02-26 21:45:34 +0900

    Update plots.R

--- SAIGE/plots.R
+++ SAIGE/plots.R
@@ -7,6 +7,6 @@ png(file='manhattan_plot.png',width=1000, height=1000)
 manhattan(gwas, main='Chromosome 1 Manhattan Plot')
 dev.off()
 
-png(file='manhattan_plot.png',width=1000, height=1000)
+png(file='qq_plot.png',width=1000, height=1000)
 qq(gwas$P, main='Q-Q plot of chromosome 1')
 dev.off()
